1580f8128dc728a351f74ed18838a09c7c8f0704
Innos <victor.d.kazakov@gmail.com>
2015-05-18 00:36:11 +0300

    added scaffold

---
+++ CSharp Advanced Teamwork Project/GameScaffold/GameScaffold.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+class Game
+{
+    struct Player
+    {
+
+    }
+    struct Enemies
+    {
+
+    }
+    struct Terrain
+    {
+
+    }
+    struct Position
+    {
+
+    }
+
+    //Static declaration so it's easily accesible
+    //Declaration
+    //declare player object
+    //declare terrain (list of the struct type, fill them upon initialization)
+    //declare enemy objects (list of the struct type, fill them upon initialization)
+    //movement
+    //attack
+    //enemy movement
+
+    static void Main()
+    {
+
+        // Console settings and GUI Initialization
+        //hide cursor
+        //set absolute height and width
+        //set GUI height and width
+        //set playing board height and width
+        //draw player stats (health, damage etc.)
+        //draw score
+
+        //Initialization
+        //player can be initialized entirely with a constructor in the struct
+        //randomly create between 0 and X terrain objects (generate random char from a list for shape within the constuctor)
+        //randomly set terrain position (check that it isn't within 1 square to the player and not on top of itself)
+        //randomly generate between 0 and Y enemy objects (colors and shape will be created by the constructor of the class)
+        //randomly set enemies starting positions ( check it isn't near the player and not on top of terrain and not on top of itself)
+
+        //while(true) running part of the game
+
+        //GUI Refresh
+        //change GUI values each tick
+
+        //Read User Key
+        //Declare movement
+        //Declare Attack
+
+        //AI Enemy Movement
+        //Enemy Movemen
+
+        //CheckForCollision
+        //Player Collision
+        //Collision with terrain and out of bounds
+        //Collision with enemies
+        //Enemy Collision
+        //COllision with terrain and out of bounds
+        //Collision with other enemies and their previous positions
+
+        //GameOver
+
+
+        //Thread Sleep (Game Speed)
+    }
+    static void InitializeObjects()
+    {
+    }
+
+    static void ReadUserKey()
+    {
+    }
+
+    //GUI
+    static void RefreshGUI()
+    {
+    }
+
+    //Colision
+    static void CheckForCollision()
+    {
+    }
+    static void CollisionEnemies()
+    {
+    }
+    static void CollisionTerrain()
+    {
+    }
+
+    //Game Over
+    static void GameOver()
+    {
+    }
+}
